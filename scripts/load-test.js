1167b65c973ec06b431b68d64d7e7e3efb541004
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-03-28 21:46:16 +0000

    feat: implement multiple scenarios

--- scripts/load-test.js
+++ scripts/load-test.js
@@ -2,12 +2,40 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 5,
-  duration: '5m',
+  scenarios: {
+    homepage: {
+      executor: 'constant-vus',
+      exec: 'homepage',
+      vus: 1,
+      duration: '5s',
+    },
+    login: {
+      executor: 'constant-vus',
+      exec: 'login',
+      vus: 1,
+      duration: '5s',
+      startTime: '5s',
+    },
+  },
 };
 
-export default function () {
-  const res = http.get('https://www.google.com');
+export function homepage () {
+  const res = http.get('http://test.k6.io');
+
+  check(res, {
+    'status is 200': r => r.status === 200,
+  });
+
+  sleep(1);
+}
+
+export function login () {
+  const params = {
+    token: 'token',
+    id: '42',
+  }
+
+  const res = http.get('http://test.k6.io/login', params);
 
   // Check if the result is a 200 or count it as failure
   check(res, {
